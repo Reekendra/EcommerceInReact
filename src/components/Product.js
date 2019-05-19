617f80d2ccd189d4730d3dbc0c2b13323230484c
Reekendra yadav <reekendray461@gmail.com>
2019-05-19 14:15:06 +0530

    first commit

---
+++ src/components/Product.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../Context";
+export default class Product extends Component {
+  render() {
+    const { id, title, img, price, inCart } = this.props.product;
+
+    return (
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() =>
+              console.log("You clicked me one the image container")
+            }
+          >
+            <Link to="/details">
+              <img src={img} alt="Image" className="card-img-top" />
+            </Link>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
+  }
+}
+
+const ProductWrapper = styled.div``;
