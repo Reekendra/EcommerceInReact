cd31ab489fca6dc0e39392848c5e240698457643
Reekendra yadav <reekendray461@gmail.com>
2019-05-19 17:17:35 +0530

    development continue...

--- src/Context.js
+++ src/Context.js
@@ -7,14 +7,30 @@ const ProductContext = React.createContext();
 //Consumer
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
+  };
+
+  componentDidMount() {
+    this.setProducts();
+  }
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return {
+        products: tempProducts
+      };
+    });
   };
   handleDetail = () => {
     console.log("Hello from detail");
   };
-  addToCart = () => {
-    console.log("Hello from add to cart");
+  addToCart = id => {
+    console.log(`Hello from add to cart id is ${id} `);
   };
   render() {
     return (
